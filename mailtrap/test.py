f8b3c493427ec4b926f050f4388b03a530f301c8
Valentin Brosseau <c4software@gmail.com>
2022-07-04 19:25:47 +0200

    Ajout script de test pour validation du mailhog

---
+++ mailtrap/test.py
@@ -0,0 +1,17 @@
+# Import smtplib for the actual sending function
+import smtplib
+
+# Import the email modules we'll need
+from email.mime.text import MIMEText
+
+msg = MIMEText("Contenu email")
+
+me = "moi@chevrollier.fr"
+you = "random@chevrollier.fr"
+msg['Subject'] = 'Sujet email'
+msg['From'] = me
+msg['To'] = you
+
+s = smtplib.SMTP('mail.ap3.local:1025')
+s.sendmail(me, [you], msg.as_string())
+s.quit()
